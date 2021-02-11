d7e74f75d89f852c36f1628d674066efd276633d
Leonard Cohnen <leonard.cohnen@gmx.de>
2021-02-11 15:37:38 +0100

    AB#360 add mbedtls socket

--- include/ttls/mbedtls_socket.h
+++ include/ttls/mbedtls_socket.h
@@ -1,8 +1,35 @@
 #pragma once
 
+#include <unordered_map>
+
+#include "mbedtls/certs.h"
+#include "mbedtls/config.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/debug.h"
+#include "mbedtls/entropy.h"
+#include "mbedtls/error.h"
+#include "mbedtls/net_sockets.h"
+#include "mbedtls/platform.h"
+#include "mbedtls/ssl.h"
 #include "socket.h"
 
 namespace edgeless::ttls {
+
+class MbedtlsContext {
+ public:
+  mbedtls_net_context server_fd;
+  mbedtls_ssl_context ssl;
+  mbedtls_ssl_config conf;
+  mbedtls_x509_crt cacert;
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_entropy_context entropy;
+
+  MbedtlsContext();
+  ~MbedtlsContext();
+
+ private:
+  std::string static readCert(const char* filename);
+};
 
 class MbedtlsSocket : public Socket {
  public:
@@ -10,6 +37,9 @@ class MbedtlsSocket : public Socket {
   int Connect(int sockfd, const sockaddr* addr, socklen_t addrlen) override;
   ssize_t Recv(int sockfd, void* buf, size_t len, int flags) override;
   ssize_t Send(int sockfd, const void* buf, size_t len, int flags) override;
+
+ private:
+  std::unordered_map<int, MbedtlsContext*> m;
 };
 
 }  // namespace edgeless::ttls
--- src/mbedtls_socket.cc
+++ src/mbedtls_socket.cc
@@ -1,21 +1,97 @@
 #include "mbedtls_socket.h"
 
 #include <mbedtls/ssl.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace edgeless::ttls;
 
-int MbedtlsSocket::Close(int /*fd*/) {
+int MbedtlsSocket::Close(int sockfd) {
+  MbedtlsContext* ctx = m[sockfd];
+  mbedtls_ssl_close_notify(&ctx->ssl);
+  m.erase(sockfd);
   return -1;
 }
 
-int MbedtlsSocket::Connect(int /*sockfd*/, const sockaddr* /*addr*/, socklen_t /*addrlen*/) {
+int MbedtlsSocket::Connect(int sockfd, const sockaddr* addr, socklen_t addrlen) {
+  auto ctx = m.insert({sockfd, new MbedtlsContext()}).first->second;
+
+  // extract hostname and port of sockaddr
+  auto hbuf = new std::string[NI_MAXHOST];
+  auto sbuf = new std::string[NI_MAXSERV];
+  getnameinfo(addr, addrlen, hbuf->data(), NI_MAXHOST, sbuf->data(),
+              NI_MAXSERV, static_cast<unsigned int>(NI_NUMERICHOST) | static_cast<unsigned int>(NI_NUMERICSERV));
+
+  mbedtls_net_connect(&ctx->server_fd, hbuf->data(), sbuf->data(), MBEDTLS_NET_PROTO_TCP);
+
+  mbedtls_ssl_config_defaults(&ctx->conf,
+                              MBEDTLS_SSL_IS_CLIENT,
+                              MBEDTLS_SSL_TRANSPORT_STREAM,
+                              MBEDTLS_SSL_PRESET_DEFAULT);
+
+  /* OPTIONAL is not optimal for security,
+     * but makes interop easier in this simplified example */
+  mbedtls_ssl_conf_authmode(&ctx->conf, MBEDTLS_SSL_VERIFY_OPTIONAL);
+  mbedtls_ssl_conf_ca_chain(&ctx->conf, &ctx->cacert, nullptr);
+  mbedtls_ssl_conf_rng(&ctx->conf, mbedtls_ctr_drbg_random, &ctx->ctr_drbg);
+  //mbedtls_ssl_conf_dbg(&ctx->conf, my_debug, stdout);
+  mbedtls_ssl_setup(&ctx->ssl, &ctx->conf);
+  mbedtls_ssl_set_hostname(&ctx->ssl, hbuf->data());
+  mbedtls_ssl_set_bio(&ctx->ssl, &ctx->server_fd, mbedtls_net_send, mbedtls_net_recv, nullptr);
+  mbedtls_ssl_handshake(&ctx->ssl);
+  mbedtls_ssl_get_verify_result(&ctx->ssl);
   return -1;
 }
 
-ssize_t MbedtlsSocket::Recv(int /*sockfd*/, void* /*buf*/, size_t /*len*/, int /*flags*/) {
+ssize_t MbedtlsSocket::Recv(int sockfd, void* buf, size_t len, int /*flags*/) {
+  auto ctx = m[sockfd];
+  mbedtls_ssl_read(&ctx->ssl, static_cast<unsigned char*>(buf), len);
   return -1;
 }
 
-ssize_t MbedtlsSocket::Send(int /*sockfd*/, const void* /*buf*/, size_t /*len*/, int /*flags*/) {
+ssize_t MbedtlsSocket::Send(int sockfd, const void* buf, size_t len, int /*flags*/) {
+  auto ctx = m[sockfd];
+  mbedtls_ssl_write(&ctx->ssl, static_cast<const unsigned char*>(buf), len);
   return -1;
+}
+
+MbedtlsContext::MbedtlsContext() : server_fd{}, ssl{}, conf{}, cacert{}, ctr_drbg{}, entropy{} {
+  mbedtls_net_init(&server_fd);
+  mbedtls_ssl_init(&ssl);
+  mbedtls_ssl_config_init(&conf);
+  mbedtls_x509_crt_init(&cacert);
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  mbedtls_entropy_init(&entropy);
+
+  mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                        nullptr,
+                        0);
+
+  std::string cert = readCert("cert.pem");
+  mbedtls_x509_crt_parse(&cacert, (const unsigned char*)cert.c_str(),
+                         sizeof(cert.c_str()));
+}
+
+MbedtlsContext::~MbedtlsContext() {
+  mbedtls_net_free(&server_fd);
+  mbedtls_x509_crt_free(&cacert);
+  mbedtls_ssl_free(&ssl);
+  mbedtls_ssl_config_free(&conf);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
+}
+
+std::string MbedtlsContext::readCert(const char* filename) {
+  std::ifstream infile(filename);
+  auto cert = std::ostringstream{};
+  if (infile) {
+    cert << infile.rdbuf();
+    infile.close();
+    return (cert.str());
+  }
+  return "";
 }
