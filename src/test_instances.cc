c6b3fe3d8a2c479ba41de1e09eb852f5ec48890a
Leonard Cohnen <lc@edgeless.systems>
2021-05-28 12:09:25 +0200

    fix thread creation

--- src/test_instances.cc
+++ src/test_instances.cc
@@ -26,7 +26,9 @@ static void Notify(void* event) {
 
 std::thread ttls::StartTestServer(const std::string& port, int client_auth) {
   Event ev{};
-  std::thread t1(edgeless_ttls_test_server, Notify, &ev, SERVER_CRT.c_str(), CA_CRT.c_str(), SERVER_KEY.c_str(), port.c_str(), client_auth);
+  std::thread t1([=, &ev] {
+    edgeless_ttls_test_server(Notify, &ev, SERVER_CRT.c_str(), CA_CRT.c_str(), SERVER_KEY.c_str(), port.c_str(), client_auth);
+  });
   {
     std::unique_lock<std::mutex> lk(ev.m);
     ev.cv.wait(lk, [&] { return ev.ready; });
@@ -35,5 +37,7 @@ std::thread ttls::StartTestServer(const std::string& port, int client_auth) {
 }
 
 std::thread ttls::StartTestClient(const std::string& port, bool client_auth) {
-  return std::thread(edgeless_ttls_test_client, CLIENT_CRT.c_str(), CA_CRT.c_str(), CLIENT_KEY.c_str(), port.c_str(), client_auth);
+  return std::thread([=] {
+    edgeless_ttls_test_client(CLIENT_CRT.c_str(), CA_CRT.c_str(), CLIENT_KEY.c_str(), port.c_str(), client_auth);
+  });
 }
